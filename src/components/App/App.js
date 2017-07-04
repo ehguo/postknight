813e3e32205714bdad9f94d44c7ff9e20ded94b4
ehguo <edward.h.guo@gmail.com>
2017-07-03 19:08:56 -0700

    initial app setup

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import StatCalculator from '../StatCalculator';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="app">
+        <StatCalculator />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/redux/modules/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import statCalculator from './statCalculator';
+
+export default combineReducers({
+  statCalculator,
+});
