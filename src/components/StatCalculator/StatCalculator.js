dcfea003c96681ba7fd3385c1a3db717c6f180a3
Edward Guo <edward.guo@walmartlabs.com>
2017-10-11 13:06:33 -0700

    Testing glamorous

--- src/components/StatCalculator/StatCalculator.js
+++ src/components/StatCalculator/StatCalculator.js
@@ -1,35 +1,43 @@
 import React, { PureComponent } from 'react';
 import R from 'ramda';
 import cn from 'classnames';
+import glamorous, { Div, Button } from 'glamorous';
 import { Counter } from './components';
 import { Stats } from 'redux/modules/statCalculator';
 
+const statCalculatorStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  outline: '#492F1C',
+  border: '3px solid #6D431B',
+  padding: '5px'
+}
+
+const pointDisplayStyle = {
+  fontSize: '1em',
+  fontFamily: 'Roboto',
+  color: '#444444'
+}
+
+const counterWrapperStyle = {
+  display: 'flex',
+  flexDirection: 'row'
+}
+
+const resetButtonStyle = {
+  padding: '5px',
+  border: 'none',
+  width: '100px',
+  background: '#444444',
+  fontFamily: 'Roboto',
+  color: '#FFFFFF',
+}
+
 export default class StatCalculator extends PureComponent {
   render() {
-    const containerStyles = cn(
-      'flex-col',
-      'flex-center',
-      'outline-darkbrown',
-      'border-brown',
-      'bg-lightbrown',
-      'p-5'
-    );
-    const pointStyles = cn(
-      'fontsize-10',
-      'roboto',
-      'grey'
-    );
-    const counterContainerStyles = cn('flex-row');
-    const resetStyles = cn('width-100',
-      'roboto',
-      'bg-grey',
-      'white',
-      'border-none',
-      'p-5'
-    );
-
     const stats = ['str', 'agi', 'int', 'vit'];
-
     const renderCounter = (stat) => (
       <Counter
         key={stat}
@@ -41,11 +49,19 @@ export default class StatCalculator extends PureComponent {
     
     const counters = R.map(renderCounter, stats);
     return (
-      <div className={containerStyles}>
-        <div className={pointStyles}>Points: {this.props.points}</div>
-        <div className={counterContainerStyles}>{counters}</div>
-        <button className={resetStyles} onClick={this.props.reset}>Reset</button>
-      </div>
+      <Div css={statCalculatorStyle}>
+        <Div css={pointDisplayStyle}>
+          Points: {this.props.points}
+        </Div>
+        <Div css={counterWrapperStyle}>
+          {counters}
+        </Div>
+        <Button
+          css={resetButtonStyle}
+          onClick={this.props.reset}>
+          Reset
+        </Button>
+      </Div>
     );
   }
 }
